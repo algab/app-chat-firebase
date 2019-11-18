5b3ab4d8ace13f933d988c004a3402687971bf34
Álvaro Oliveira <alvarogab6@gmail.com>
2019-11-18 14:34:01 -0300

    remove react-native-firebase, add firebase js sdk

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,22 +1,16 @@
-import firebase from 'react-native-firebase';
+import * as firebase from 'firebase';
 import {
     API_KEY,
     AUTH_DOMAIN,
     DATABASE_URL,
-    PROJECT_ID,
     STORAGE_BUCKET,
-    MESSAGING_SENDER_ID,
-    APP_ID,
 } from 'react-native-dotenv';
 
 const connection = firebase.initializeApp({
     apiKey: API_KEY,
     authDomain: AUTH_DOMAIN,
     databaseURL: DATABASE_URL,
-    projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID,
 });
 
 export default connection;
